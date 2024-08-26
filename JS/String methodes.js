354932b6020032659c610e6c1c364f42ddcb9f9b
PRASANTH9908 <573prasanth@gmail.com>
2024-08-26 16:35:27 +0530

    Add files via upload

---
+++ JS/String methodes.js
@@ -0,0 +1,62 @@
+//STRING METHODES//
+/*
+var text="welcome"
+let a=text.charAt(2);
+console.log(a)
+
+let q="hello world"
+let w=q.toUpperCase();
+console.log(w)
+
+let e="PRASANTH"
+let r=e.toLowerCase();
+console.log(r)
+ 
+var t="ishoespeed"
+var y=t.length;
+console.log(y);
+
+
+var s="P" ;
+var s=s.repeat(10);
+console.log(s);
+
+var u="mobile"
+var u=u.replace("mobile","car")
+console.log(u)
+
+var o="1,2,3,4,5"
+var o=o.substring(0,3)
+console.log(o)
+
+
+var T="theepalapudi"
+var P="prasanth"
+var name=T.concat(" ",P)
+console.log(name)
+
+
+var d="       prasanth       "
+//var d=d.trim();
+var d=d.length;
+console.log(d);
+
+var f="  theepalapudi  "
+var d=f.length;
+var f=f.trimStart();
+console.log(f);
+console.log(d);
+
+var h="hello"
+var h=h.split("")
+console.log(h)
+
+
+var f="kalki"
+var f=f.split("")
+console.log(f)
+
+var v="sir your are great,sir your  genius,sir your gentlemen"
+var v=v.replaceAll("sir","prasanth");
+console.log(v);
+*/
